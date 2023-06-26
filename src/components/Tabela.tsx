d0d6330f7e42b2d09ee03fdf2bf88c4aaf57a82a
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-26 20:52:17 -0300

    Ícones locais de edição e exclusão

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,4 +1,5 @@
 import Cliente from "@/core/Cliente";
+import { IconeEdicao, IconeLixo } from "./Icones";
 
 interface TabelaProps {
     clientes: Cliente[],
@@ -12,8 +13,24 @@ export default function Tabela(props: TabelaProps) {
                     <th className="text-left p-4">Código</th>
                     <th className="text-left p-4">Nome</th>
                     <th className="text-left p-4">Idade</th>
+                    <th className=" p-4">Ações</th>
                 </tr>
             </>
+        )
+    }
+
+    function renderizarAcoes(cliente: Cliente) {
+        return (
+            <td className="flex">
+                <button className={`
+                    flex justify-center items-center text-green-600 hover:bg-purple-50
+                    rounded-full p-2 m-1
+                `}>{IconeEdicao}</button>
+                <button className={`
+                    flex justify-center items-center text-red-600 hover:bg-purple-50
+                    rounded-full p-2 m-1
+                `}>{IconeLixo}</button>
+            </td>
         )
     }
 
@@ -21,13 +38,14 @@ export default function Tabela(props: TabelaProps) {
         return props.clientes?.map((cliente, i) => {
             return (
                 <>
-                    <tr key={cliente.id} 
-                    className={`
+                    <tr key={cliente.id}
+                        className={`
                         ${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}
                     `}>
                         <td className="text-left p-4">{cliente.id}</td>
                         <td className="text-left p-4">{cliente.nome}</td>
                         <td className="text-left p-4">{cliente.idade}</td>
+                        {renderizarAcoes()}
                     </tr>
                 </>
             )
